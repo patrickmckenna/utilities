90eb26bd57e50cc7237f1fba8848849616591d07
Patrick McKenna <patrick.b.mckenna@gmail.com>
2015-06-01 13:42:46 -0700

    more complete default script

--- new_script.sh
+++ new_script.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
-
-# create new bash script in current directory, then open it in your editor
 # use: ./new_script <scriptname>
+#!/bin/bash
+# Usage: new_script <script_name>
+# create new bash script in current directory, then open it in your editor
 
-# check that name given
+set -e
+
+# check that script_name given
 if [[ ! $1 ]]; then
   echo "No filename given!"
   exit 1
 fi
-
 scriptname="$1"
 
 # check if file of same name already exists
@@ -17,10 +18,19 @@ if  [[ -e $scriptname ]]; then
   exit 1
 fi
 
-# create new file with shebang
-echo '#!/bin/bash' >> "$scriptname"
+# create new script template
+cat << EOF > "$scriptname"
+#!/bin/bash
+
+# Usage: $scriptname <required_arg> [<optional_arg>] [--optional-flag]
+# brief description of the script's purpose
+
+set -e
+EOF
+
 # make new file executable
 chmod +x "$scriptname"
+
 # open new file in editor
 if [[ $EDITOR ]]; then
   $EDITOR "$scriptname"
