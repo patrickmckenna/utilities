dd781049415969247d496cc42d01d7209a4d4e65
Patrick McKenna <patrick.b.mckenna@gmail.com>
2015-06-23 10:51:54 -0700

    change names, too

--- rewrite_commit_author.sh
+++ rewrite_commit_author.sh
@@ -8,12 +8,22 @@ set -e
 git filter-branch --env-filter '
   if test "$GIT_AUTHOR_EMAIL" = "patrickbm@github.com"
   then
-    GIT_AUTHOR_EMAIL="patrick.b.mckenna@gmail.com"
+    GIT_AUTHOR_EMAIL="trainingdemos+githubteacher@github.com"
     export GIT_AUTHOR_EMAIL
+  fi
+  if test "$GIT_AUTHOR_NAME" = "Patrick McKenna"
+  then
+    GIT_AUTHOR_NAME="githubteacher"
+    export GIT_AUTHOR_NAME
   fi
   if test "$GIT_COMMITTER_EMAIL" = "patrickbm@github.com"
   then
-    GIT_COMMITTER_EMAIL="patrick.b.mckenna@gmail.com"
+    GIT_COMMITTER_EMAIL="trainingdemos+githubteacher@github.com"
     export GIT_COMMITTER_EMAIL
+  fi
+  if test "$GIT_COMMITTER_NAME" = "Patrick McKenna"
+  then
+    GIT_COMMITTER_NAME="githubteacher"
+    export GIT_COMMITTER_NAME
   fi
 ' -- --all
