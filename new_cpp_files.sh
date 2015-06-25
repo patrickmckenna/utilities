248d27505218f54f89c938fa1d27f3a14b8f25dc
Patrick McKenna <patrick.b.mckenna@gmail.com>
2015-06-25 08:22:20 -0700

    fix up tests and comments

--- new_cpp_files.sh
+++ new_cpp_files.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 # Usage: new_cpp_files <filename>
-# create new cpp file, then open it in your editor
+# create new C++ header and implementation files, then open them in your editor
 
 set -e
 
-# check that script_name given
-[ $1 ] || echo "No filename given!" && exit 1
+# check that a filename was passed
+[ $1 ] || (echo "No filename given!" && exit 1)
 filename="$1"
 header="${filename}.hh"
 implementation="${filename}.cc"
 
-# check if file of same name already exists
-if  [[ -e $header ]]; then
-  echo "File $header already exists!"
-  exit 1
-fi
+# check if files of same name already exist
+[ -e $header ] && (echo "File $header already exists!" && exit 1)
+[ -e $implementation ] && (echo "File $implementation already exists!" && exit 1)
 
-# create new script template
+# create header file
 cat << EOF > "$header"
 #if !defined __${filename}_hh__
 #define __${filename}_hh__
