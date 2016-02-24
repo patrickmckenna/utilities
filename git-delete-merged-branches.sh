8db694a3b2b0ccce13959a8446c0e58322b9d48b
Patrick McKenna <patrickmckenna@github.com>
2016-02-24 13:44:34 -0800

    hacky first pass at auto-detecting default branch when deleting merged branches

--- git-delete-merged-branches.sh
+++ git-delete-merged-branches.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Usage: git-delete-merged-branches.sh
-# delete all local branches that have been merged into master
-# does not delete currently checked-out branch or master itself
+# deletes all local branches, except for the default and the currently checked out branch(es), that have been merged into the default branch
+# default here means GitHub default branch, not the target of refs/remotes/origin/HEAD
+# assumes your remote is named "origin"
+# asuumes you have a personal access token available via an environment variable called GITHUB_TOKEN
+# assumes you have jq installed
 
 set -e
 
-git branch --merged master | grep -v -e "\*" -e "master" | xargs -n 1 git branch -d
+remote_url=$(git remote get-url origin)
+default_branch=$(curl -H "Authorization:token $GITHUB_TOKEN" https://api.github.com/repos/$remote_url | jq -r '.default_branch')
+
+git branch --merged $default_branch | grep -v -e "\*" -e $default_branch | xargs -n 1 git branch -d
 
 exit 0
