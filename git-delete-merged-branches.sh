b5619f983ea2081d32b36898556134f7d8800dfa
Patrick McKenna <patrickmckenna@github.com>
2016-02-26 14:57:30 -0800

    use vars, not hard-coded strings

--- git-delete-merged-branches.sh
+++ git-delete-merged-branches.sh
@@ -9,8 +9,12 @@
 
 set -e
 
-remote_url=$(git remote get-url origin)
-default_branch=$(curl -H "Authorization:token $GITHUB_TOKEN" https://api.github.com/repos/$remote_url | jq -r '.default_branch')
+https_url="https:\/\/:github.com\/"
+ssh_url="git@github.com:"
+remote_name="origin"
+user_repo=$(git remote get-url $remote_name | sed s/$https_url// | sed s/$ssh_url// | sed s/.git//)
+
+default_branch=$(curl -H "Authorization:token $GITHUB_TOKEN" https://api.github.com/repos/$user_repo | jq -r '.default_branch')
 
 git branch --merged $default_branch | grep -v -e "\*" -e $default_branch | xargs -n 1 git branch -d
 
