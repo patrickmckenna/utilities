808aaf30f1d82b1e5c6fad89f3c07e4b35255d6b
Patrick McKenna <patrick.b.mckenna@gmail.com>
2016-05-12 14:11:16 -0700

    update python packages, too

--- update-all.sh
+++ update-all.sh
@@ -4,6 +4,11 @@
 
 set -e
 
-brew update && brew upgrade && brew cleanup && brew prune
+brew update
+brew upgrade
+brew cleanup
+brew prune
 
 gem update
+
+pip3 list | xargs -n 1 pip3 install --upgrade
