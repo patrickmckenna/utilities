7f95438d47d5bd1787a9aa7b03848b4937a75707
Patrick McKenna <patrick.b.mckenna@gmail.com>
2016-10-09 12:32:24 -0300

    keep local copy of python docs up to date

--- update-all.sh
+++ update-all.sh
@@ -15,3 +15,11 @@ pip install --upgrade pip setuptools
 pip3 install --upgrade pip setuptools wheel
 pip list --outdated | cut -d \( -f 1 | xargs -n 1 pip install --upgrade
 pip3 list --outdated | cut -d \( -f 1 | xargs -n 1 pip3 install --upgrade
+
+# keep the latest copy of Python docs available locally
+cd "$HOME/Documents"
+PYTHON3_VERSION=$(python3 -V | sed s/Python\ //)
+ARCHIVED_DOCS="python-$PYTHON3_VERSION-docs-html.tar.bz2"
+curl -O "https://docs.python.org/3/archives/$ARCHIVED_DOCS"
+tar -xf "$ARCHIVED_DOCS"
+rm "$ARCHIVED_DOCS"
