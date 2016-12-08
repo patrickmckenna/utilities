f611619c40b0fee52e531ccfe1f006ed4892973d
Patrick McKenna <patrick.b.mckenna@gmail.com>
2016-12-08 11:19:56 +0100

    keep updated local copy of MySQL docs
    This is ugly, but works for now.

--- update-all.sh
+++ update-all.sh
@@ -21,10 +21,22 @@ pip3 list --outdated | cut -d \( -f 1 | xargs -n 1 pip3 install --upgrade
 # update Atom packages
 apm upgrade -c false
 
-# keep the latest copy of Python docs available locally
-cd "$HOME/Documents"
+# keep the latest copy of Python, MySQL docs available locally
+DOCS_HOME="$HOME/Documents"
+cd "$DOCS_HOME"
+
 PYTHON3_VERSION=$(python3 -V | sed s/Python\ //)
-ARCHIVED_DOCS="python-$PYTHON3_VERSION-docs-html.tar.bz2"
-curl -O "https://docs.python.org/3/archives/$ARCHIVED_DOCS"
-tar -xf "$ARCHIVED_DOCS"
-rm "$ARCHIVED_DOCS"
+PYTHON_DOWNLOAD="python-$PYTHON3_VERSION-docs-html.tar.bz2"
+curl -O "https://docs.python.org/3/archives/$PYTHON_DOWNLOAD"
+tar -xf "$PYTHON_DOWNLOAD"
+rm "$PYTHON_DOWNLOAD"
+
+MYSQL_VERSION="5.6"
+MYSQL_DOCS_BASEURL="http://downloads.mysql.com/docs/"
+MYSQL_DOCS="refman-$MYSQL_VERSION-en."
+MYSQL_HTML_DOCS="${MYSQL_DOCS}html-chapter.tar.gz"
+MYSQL_PDF_DOCS="${MYSQL_DOCS}pdf"
+curl -O "$MYSQL_DOCS_BASEURL$MYSQL_HTML_DOCS"
+curl -O "$MYSQL_DOCS_BASEURL$MYSQL_PDF_DOCS"
+tar -xf "$MYSQL_HTML_DOCS"
+rm "$MYSQL_HTML_DOCS"
