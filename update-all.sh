8076213a19d91562f5b7088a83467fab5e533e77
Patrick McKenna <patrick.b.mckenna@gmail.com>
2017-01-16 11:44:50 -0800

    Revert "keep updated local copy of MySQL docs"
    This reverts commit f611619c40b0fee52e531ccfe1f006ed4892973d.
    
    Haven't needed these.

--- update-all.sh
+++ update-all.sh
@@ -23,16 +23,3 @@ pip3 list --outdated | cut -d \( -f 1 | xargs -n 1 pip3 install --upgrade
 
 # update Atom packages
 apm upgrade -c false
-
-# keep the latest copy of MySQL docs available locally
-DOCS_HOME="$HOME/Documents"
-cd "$DOCS_HOME"
-MYSQL_VERSION="5.6"
-MYSQL_DOCS_BASEURL="http://downloads.mysql.com/docs/"
-MYSQL_DOCS="refman-$MYSQL_VERSION-en."
-MYSQL_HTML_DOCS="${MYSQL_DOCS}html-chapter.tar.gz"
-MYSQL_PDF_DOCS="${MYSQL_DOCS}pdf"
-curl -O "$MYSQL_DOCS_BASEURL$MYSQL_HTML_DOCS"
-curl -O "$MYSQL_DOCS_BASEURL$MYSQL_PDF_DOCS"
-tar -xf "$MYSQL_HTML_DOCS"
-rm "$MYSQL_HTML_DOCS"
