ea46b71428932a6a84fa725b534359aae3d802bf
Patrick McKenna <patrick.b.mckenna@gmail.com>
2017-03-27 09:29:15 -0700

    reinstall shims after Ruby/Python package updates

--- update-all.sh
+++ update-all.sh
@@ -14,11 +14,13 @@ brew prune
 brew bundle dump --force --file="$HOME/projects/dotfiles/Brewfile"
 
 gem update
+rbenv rehash
 
 pip install --upgrade pip setuptools
 pip3 install --upgrade pip setuptools wheel
 pip list --outdated | cut -f 1 -d ' ' | grep --invert-match --extended-regexp --regexp="Package|-------" | xargs -n 1 pip install --upgrade
 pip3 list --outdated | cut -f 1 -d ' ' | grep --invert-match --extended-regexp --regexp="Package|-------" | xargs -n 1 pip3 install --upgrade
+pyenv rehash
 
 # update Atom packages
 apm upgrade --confirm false
