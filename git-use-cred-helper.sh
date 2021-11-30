7a6be2163317ceb1cd001a011f0825931a1be256
Patrick McKenna <patrick.b.mckenna@gmail.com>
2021-11-29 16:48:48 -0800

    don't need these

--- git-use-cred-helper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Usage: git-use-cred-helper.sh
-# temporarily use git credential helper for a given repo
-
-set -e
-
-git config --local credential.helper cache --timeout=36000
--- set-terminal-title.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# useage: set_terminal_title <title>
-echo -ne "\033]0;"$@"\007"
