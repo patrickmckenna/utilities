0163956bec62e4bf4a43c9a1c87772b52d1a1255
Patrick McKenna <patrick.b.mckenna@gmail.com>
2021-11-30 18:13:08 -0800

    simplify help messages

--- template.sh
+++ template.sh
@@ -7,9 +7,9 @@
 set -e
 
 # if help requested, print the comments at the top of the file
-if [[ "$1" == "--help" || "$1" == "-h" || "$1" == "help" ]]; then
+[[ "$1" =~ -h|(--)?help ]] && {
   grep "^#/" < "$0" | cut -c4-
   exit 0
-fi
+}
 
 echo "==> A relevant message"
